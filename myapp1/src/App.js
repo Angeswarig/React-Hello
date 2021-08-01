f76697d67e623d1a5b29721ccfd54620cb55fcc0
Angeswarig <angeswarig97@gmail.com>
2021-08-01 18:34:05 +0530

    files added

---
+++ myapp1/src/App.js
@@ -0,0 +1,29 @@
+import React,{useState} from "react"
+import "./style.css";
+import Button from "./Button";
+
+const App= () =>{
+    
+    return (
+    <div>
+     <h1 style={{ textAlign:"center" }}>HELLO,This is Angeswari G!!</h1>
+    <Button title="App Store"/>
+    <Button title="Play Store" /> 
+
+    
+    {/* <button className="button">App  Store</button>
+    <button className="button">Play  Store</button> */}
+
+    
+
+
+
+
+
+    
+    </div>
+    );
+}
+
+export default App;
+
